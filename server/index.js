01f3bca07f5ce9edf2cfc75c7f82ca785f3bee0c
karthikt <karthik.thatipelly96@gmail.com>
2022-04-14 10:16:02 +0530

    first commit in pizza store

---
+++ server/index.js
@@ -0,0 +1,29 @@
+//Making server online
+
+const express = require("express");
+const PORT= process.env.PORT || 3001;
+const app = express();
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
+
+app.get('/', (req,res) => res.send('The Pizza Store is online'));
+
+//Connecting to database
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('pizzastore','postgres','postgres',{
+dialect: 'postgres',
+host:'localhost',
+port:5425
+})
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
